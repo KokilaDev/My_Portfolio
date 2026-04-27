a0ab9979564a671ff967ec2f342ff7dcbea205ea
KokilaDev <kokiladewmini527@gmail.com>
2026-04-27 14:57:31 +0530

    create header bar

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector('.toggle_btn');
+const dropdownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('open');
+});
