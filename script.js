5d0f63eeffff96e129eb41dcbf1e1259c17ae191
KokilaDev <kokiladewmini527@gmail.com>
2026-05-01 10:43:12 +0530

    add responsive for hero section

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 // Header
 const toggleBtn = document.querySelector('.toggle_btn');
 const dropdownMenu = document.querySelector('.dropdown_menu');
+const toggleIcon = toggleBtn.querySelector('i');
 
 toggleBtn.addEventListener('click', () => {
     dropdownMenu.classList.toggle('open');
+
+    if (dropdownMenu.classList.contains('open')) {
+        toggleIcon.classList.remove('fa-bars');
+        toggleIcon.classList.add('fa-times');
+    } else {
+        toggleIcon.classList.remove('fa-times');
+        toggleIcon.classList.add('fa-bars');
+    }
 });
 
 // Hero
