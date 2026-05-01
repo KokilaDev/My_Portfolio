df535ea0b0692d5d1a20636d5e457537c3081328
KokilaDev <kokiladewmini527@gmail.com>
2026-05-01 10:07:20 +0530

    update hero section

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+// Header
 const toggleBtn = document.querySelector('.toggle_btn');
 const dropdownMenu = document.querySelector('.dropdown_menu');
 
 toggleBtn.addEventListener('click', () => {
     dropdownMenu.classList.toggle('open');
 });
+
+// Hero
+var typed = new Typed('.auto-type', {
+    strings: [
+        "<span class='front'>Frontend Developer</span>",
+        "<span class='back'>Backend Developer</span>",
+        "<span class='soft'>Software Engineer</span>"
+    ],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+    contentType: 'html'
+});
+
+document.getElementById("btn_explore").addEventListener("click", function () {
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    })
+})
